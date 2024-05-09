caf200c895567d796285c3db7e2a8816167c4db3
Soumyajit Naskar <soumyajit13102003@gmail.com>
2024-05-09 19:38:58 +0530

    Learning arrays and slices

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// fmt.Println("Hello World!!")
@@ -23,6 +25,24 @@ func main() {
 	// Also general thing to note is that just declare variables like var variableName = "anything"/anything because go will infer it
 
 	var savedString = fmt.Sprintf("My name is %v, and my score is %v", nameOne, marks)
-	fmt.Printf(savedString)
+	fmt.Println(savedString)
+
+	// Declaring an array
+	var arr = [3]int{2, 3, 5}
+	names := [3]string{"Atanu", "Soumyajit", "Tuhin"}
+	names[1] = "Arith"
+	fmt.Println(arr, len(arr))
+	fmt.Println(names, len(names))
+
+	// Declaring a slice it is variable len in nature
+	scores := []int32{2, 3, 4}
+	fmt.Println(scores)
+	// Some slice functions
+	scores = append(scores, 5) // overwriting
+	fmt.Println(scores, len(scores))
+	sliceRange := scores[1 : len(scores)-1]
+	sliceRangeTwo := scores[:3]
+	sliceRangeThree := scores[:]
+	fmt.Println(sliceRange, sliceRangeTwo, sliceRangeThree)
 
 }
