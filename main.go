2e7ecfc69ddab6695ce9cb3d401dad73b8aec86b
Soumyajit Naskar <soumyajit13102003@gmail.com>
2024-05-09 18:32:51 +0530

    Basic main function and variables/datatypes

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello World!!")
+	var nameOne string = "Hello"
+	var marks int = 75
+	var nameThree bool
+
+	fmt.Println(nameOne, marks, nameThree)
+}
