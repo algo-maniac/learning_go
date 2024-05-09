2a4e453cd6d63419e4c6680e8128a975e3407e69
Soumyajit Naskar <soumyajit13102003@gmail.com>
2024-05-09 18:39:53 +0530

    More on variables and datatypes

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	// fmt.Println("Hello World!!")
+
+	// We can declare variable by the following methods
+
 	var nameOne string = "Hello"
 	var marks int = 75
-	var nameThree bool
+	var nameThree bool // default value if value not mentioned
+	name := "hey"
+	var score int64 = 100000 // for higher bit size
 
+	fmt.Println(name, score)
 	fmt.Println(nameOne, marks, nameThree)
 }
